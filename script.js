b67c0ca82b0d92cbcd8739c0313fe50d655768f0
Bibek-42069 <bibekbhattarai5th@gmail.com>
2021-03-21 00:15:34 -0400

    add PlayRound Function

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-let arr = ['Rock','Paper','Scissor']
+let arr = ['Rock','Paper','Scissor'];
 
 function computerPlay(){
     return arr[Math.floor(Math.random() * Math.floor(3))]
 }
+
+function playRound(playerSel, computerSel){
+    let whoWon = ''
+
+    if(playerSel === 'Rock'){
+        if(computerSel === 'Paper')
+            whoWon = 'You lose! Paper beats rock!';
+        else if(computerSel === 'Rock')
+            whoWon = 'Tied! Rock and Rock cancel each other out!';
+        else
+            whoWon = 'You won! Rock beats scissor';
+    } 
+    else if(playerSel === 'Scissors' ) {
+        if(computerSel === 'Rock')
+            whoWon = 'You lose! Rock beats scissors';
+        else if(computerSel === 'Scissors')
+            whoWon = 'Tied! Scissor and Scissor cancel each other out';
+        else
+            whoWon = 'You won! Scissor beats Paper'
+    }
+    else{
+        if(computerSel === 'Scissor')
+            whoWon = 'You lose! Scissor beats paper!';
+        else if(computerSel === 'Paper')
+            whoWon = 'Tied! Paper and Paper cancel each other out';
+        else
+            whoWon = 'You won! Paper beats Rock!';
+    }
+    return whoWon
+}
+
+const computerSel = computerPlay();
+const playerSel = 'Paper';
+
+console.log(computerSel);
+console.log(playRound(playerSel, computerSel));
